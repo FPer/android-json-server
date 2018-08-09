15efafdb93e1b3433d660e1132a5ff65c815ebc3
nakana <t-nakanishi@esm.co.jp>
2018-08-09 13:14:51 +0900

    ボタンの挙動をつけた

--- android/First/app/src/main/java/a/fp/first/MainActivity.kt
+++ android/First/app/src/main/java/a/fp/first/MainActivity.kt
@@ -2,11 +2,21 @@ package a.fp.first
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        loadBtn.setOnClickListener {
+            textView.text = "ロードしたつもり"
+        }
+
+        resetBtn.setOnClickListener{
+            textView.text = "リセットした"
+        }
+
     }
 }
