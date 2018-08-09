7f2f7689008f153e64283e492cacec17bb728010
nakana <t-nakanishi@esm.co.jp>
2018-08-09 15:40:13 +0900

    二重起動できるようにした

--- android/First/app/src/main/java/a/fp/first/MainActivity.kt
+++ android/First/app/src/main/java/a/fp/first/MainActivity.kt
@@ -1,16 +1,27 @@
 package a.fp.first
 
+import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+    private fun goToNextActivity(){
+        val intent = Intent(this, NextActivity::class.java)
+        Handler().postDelayed({ startActivity(intent) }, 200)
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        nextBtn.setOnClickListener {
+            goToNextActivity()
+        }
+
         loadBtn.setOnClickListener {
+            Thread.sleep(10000)
             textView.text = "ロードしたつもり"
         }
 
